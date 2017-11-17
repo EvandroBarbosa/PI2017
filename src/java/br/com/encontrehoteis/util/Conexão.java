6e4519d4daa100b458f88c9afd1747eff4801f93
aluno <aluno@LAB107-19>
2017-11-17 14:10:04 -0200

    implementando a conexão

--- src/java/br/com/encontrehoteis/util/Conexão.java
+++ src/java/br/com/encontrehoteis/util/Conexão.java
@@ -1,9 +1,36 @@
 package br.com.encontrehoteis.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author aluno
  */
 public class Conexão {
-    
+
+    private static Connection conexao;
+
+    private Conexão() {
+    }
+
+    public static Connection getConexao() {
+        if (conexao != null) {
+            return conexao;
+        } else {
+            try {
+                Class.forName("Org.postgresql.Driver");
+                conexao = DriverManager.getConnection(
+                        "jdbc:postgresql://localhost:5432/encontrehoteis", "postgres", "senha");
+            } catch (ClassNotFoundException erro) {
+                System.out.println("Aplicação não contém o driver do banco");
+               return null;
+            } catch (SQLException ex) {
+                System.out.println("Erro na conexão como banco: verifique a url, o usuário e a senha");
+                return null;
+            }
+        }
+        return conexao;
+    }
 }
